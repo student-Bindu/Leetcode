01d5ce8e7ee0e5fc5fae58961c5463c2a05fe828
Bindu M <146534729+student-Bindu@users.noreply.github.com>
2024-07-30 23:07:44 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (89.28%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode curr, prev, insertionNode = null, preHead = new ListNode(-1, head);
+            
+        for (curr = head, prev = preHead; curr != null; prev = curr, curr = curr.next) {
+      
+            // initialize the insertion node with the first node greater than or equals to x
+            if (curr.val >= x && insertionNode == null) {
+                insertionNode = prev;
+                continue;
+            }
+
+             if (curr.val < x) {
+                // no node greater than or equals to X was found
+                // so we don't need to do anything
+                if (insertionNode == null) {
+                    continue;
+                }
+
+                // move current node to be after insertion node
+                prev.next = curr.next;
+                curr.next = insertionNode.next;
+                insertionNode.next = curr;
+
+                // make current node the new insertion node
+                insertionNode = curr;
+            }
+        }
+
+        return preHead.next;
+        
+    }
+}
