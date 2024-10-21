92eaabdef84d4495c158d49098c288db81fcb315
Bindu Muniyappa <bindum089@gmail.com>
2024-10-21 22:55:28 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (65.71%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,5 +1,5 @@
 /**
- * Definition for a binary tree node.
+ * Definition for a binary tree nod
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -15,16 +15,47 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        preorderHelper(root, result);
-        return result;
-    }
-    private void preorderHelper(TreeNode node, List<Integer> result) {
-        if (node == null) {
-            return;
+         List<Integer> preorder = new ArrayList<>();
+
+        // If the root is null, return
+        // an empty traversal result
+        if (root == null) {
+            return preorder;
         }
-        result.add(node.val);
-        preorderHelper(node.left, result);
-        preorderHelper(node.right, result);
+
+        // Create a stack to store
+        // nodes during traversal
+        Stack<TreeNode> st = new Stack<>();
+        // Push the root node
+        // onto the stack
+        st.push(root);
+
+        // Perform iterative preorder traversal
+        while (!st.empty()) {
+            // Get the current node
+            // from the top of the stack
+            root = st.pop();
+
+            // Add the node's value to
+            // the preorder traversal result
+            preorder.add(root.val);
+
+            // Push the right child
+            // onto the stack if exists
+            if (root.right != null) {
+                st.push(root.right);
+            }
+
+            // Push the left child onto
+            // the stack if exists
+            if (root.left != null) {
+                st.push(root.left);
+            }
+        }
+
+        // Return the preorder
+        // traversal result
+        return preorder;
     }
 }
+ 
